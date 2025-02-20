a9b37b67ef32a95c44b4dadd1b73dcde1a5c7443
Arnab Roy <royarnab056@gmail.com>
2025-02-20 20:29:45 +0530

    Connecting userdb and JournalEntrydb

--- src/main/java/com/arnab/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/arnab/journalApp/controller/JournalEntryController.java
@@ -1,15 +1,15 @@
 package com.arnab.journalApp.controller;
 
 import com.arnab.journalApp.entity.JournalEntry;
+import com.arnab.journalApp.entity.User;
 import com.arnab.journalApp.service.JournalEntryService;
+import com.arnab.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.*;
 
 
@@ -20,20 +20,23 @@ public class JournalEntryController {
     @Autowired
     private JournalEntryService journalEntryService;
 
-    @GetMapping
-    public ResponseEntity<?> getAll(){
-        List<JournalEntry> all = journalEntryService.getAll();
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{userName}")
+    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String userName){
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> all = user.getJournalEntries();
         if(all != null && !all.isEmpty()){
             return new ResponseEntity<>(all, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName){
         try{
-            myEntry.setDate(LocalDateTime.now());
-            journalEntryService.saveEntry(myEntry);
+            journalEntryService.saveEntry(myEntry ,userName);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
         catch (Exception e){
@@ -51,14 +54,14 @@ public class JournalEntryController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("id/{id}")
-    public ResponseEntity<?> deleteEntryById(@PathVariable ObjectId id){
-        journalEntryService.deleteEntryById(id);
+    @DeleteMapping("id/{userName}/{id}")
+    public ResponseEntity<?> deleteEntryById(@PathVariable ObjectId id, @PathVariable String userName){
+        journalEntryService.deleteEntryById(id, userName);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("id/{id}")
-    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
+    @PutMapping("id/{userName}/{id}")
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry, @PathVariable String userName ){
         JournalEntry old = journalEntryService.findById(id).orElse(null) ;
         if(old!=null){
             old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
--- src/main/java/com/arnab/journalApp/entity/JournalEntry.java
+++ src/main/java/com/arnab/journalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package com.arnab.journalApp.entity;
 
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -12,6 +13,7 @@ import java.util.Date;
 
 @Document(collection = "journal_entries")
 @Data
+@NoArgsConstructor
 public class JournalEntry {
 
     @Id private ObjectId id;
